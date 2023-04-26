a3a71a4fa58e9e1be5593a743f95a22d7e2c82b5
allybaubooa <allyhusseinbaubooa@gmail.com>
2023-04-26 14:50:43 +0400

    New Section Updates

--- app.py
+++ app.py
@@ -8,11 +8,6 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
 @app.route('/projects')
 def projects():
     return render_template('projects.html')
